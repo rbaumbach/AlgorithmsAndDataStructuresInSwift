921ddc7d03b8f9b9cda13d650bdeb3484792967f
Ryan <github@ryan.codes>
2018-04-11 23:35:24 -0600

    Implment SinglyLinkedList methods

--- AlgorithmsAndDataStructuresInSwift/Data Structures/SinglyLinkedList.swift
+++ AlgorithmsAndDataStructuresInSwift/Data Structures/SinglyLinkedList.swift
@@ -16,6 +16,74 @@ class SinglyLinkedList {
     }
     
     func addToFront(item: Int) {
-        head = SingleNode(item: item)
+        if let oldHead = head {
+            head = SingleNode(item: item)
+            head?.nextNode = oldHead
+        } else {
+            head = SingleNode(item: item)
+        }
+    }
+    
+    func removeFromFront() -> Int? {
+        guard let oldHead = head else {
+            return nil
+        }
+        
+        let oldHeadItem = oldHead.item
+        
+        head = oldHead.nextNode
+        
+        return oldHeadItem
+    }
+    
+    func addToBack(item: Int) {
+        if let tailNode = findTailNode() {
+            tailNode.nextNode = SingleNode(item: item)
+        } else {
+            head = SingleNode(item: item)
+        }
+    }
+    
+    func removeFromBack() -> Int? {
+        guard let head = head else {
+            return nil
+        }
+        
+        if head.nextNode == nil {
+            return head.item
+        }
+        
+        let newTailNode: SingleNode = findNodeImmediatelyBeforeTailNode()
+        let oldTailNode: SingleNode? = newTailNode.nextNode
+
+        newTailNode.nextNode = nil
+
+        return oldTailNode?.item
+    }
+    
+    // MARK: - Private Methods
+    
+    private func findTailNode() -> SingleNode? {
+        guard let head = head else {
+            return nil
+        }
+
+        var currentNode: SingleNode? = head
+
+        while currentNode?.nextNode != nil {
+            currentNode = currentNode?.nextNode
+        }
+
+        return currentNode
+    }
+    
+    private func findNodeImmediatelyBeforeTailNode() -> SingleNode {
+        var currentNode: SingleNode? = head
+
+        while currentNode?.nextNode?.nextNode != nil {
+            currentNode = currentNode?.nextNode
+        }
+
+        return currentNode!
     }
 }
--- Specs/Data Structures/SinglyLinkedListSpec.swift
+++ Specs/Data Structures/SinglyLinkedListSpec.swift
@@ -29,13 +29,149 @@ class SinglyLinkedListSpec: QuickSpec {
             }
         }
         
+        describe("#peek()") {
+            var peekedItem: Int!
+            
+            describe("when the list is empty") {
+                beforeEach {
+                    peekedItem = subject.peek()
+                }
+                
+                it("returns nil") {
+                    expect(peekedItem).to(beNil())
+                }
+            }
+            
+            describe("when the list isn't empty") {
+                beforeEach {
+                    subject.addToFront(item: 22)
+                    
+                    peekedItem = subject.peek()
+                }
+                
+                it("returns the item at the head of the subject") {
+                    expect(peekedItem).to(equal(22))
+                }
+            }
+        }
+        
         describe("#addToFront(item:)") {
             beforeEach {
                 subject.addToFront(item: 99)
             }
             
-            it("adds item to the subject") {
-                expect(subject.peek()).to(equal(99))
+            describe("when the list is empty") {
+                it("adds item to the subject") {
+                    expect(subject.peek()).to(equal(99))
+                }
+            }
+            
+            describe("when the list isn't empty") {
+                beforeEach {
+                    subject.addToFront(item: 999)
+                }
+                
+                it("adds the item to the subject at the front of the linked list") {
+                    expect(subject.peek()).to(equal(999))
+                    
+                    // the easiest way to test that it's adding a node in the front of a list is to remove it after
+                    // and verify the old head is still present
+                    
+                    _ = subject.removeFromFront()
+                    
+                    expect(subject.peek()).to(equal(99))
+                }
+            }
+        }
+        
+        describe("#removeFromFront()") {
+            var removedItem: Int!
+
+            describe("when the list is empty") {
+                beforeEach {
+                    removedItem = subject.removeFromFront()
+                }
+                
+                it("returns nil") {
+                    expect(removedItem).to(beNil())
+                }
+            }
+            
+            describe("when the list is NOT empty") {
+                beforeEach {
+                    subject.addToFront(item: 11)
+                    removedItem = subject.removeFromFront()
+                }
+                
+                it("returns the item in the front of the linked list") {
+                    expect(removedItem).to(equal(11))
+                }
+            }
+        }
+        
+        describe("#addToBack(item:)") {
+            beforeEach {
+                subject.addToBack(item: 99)
+            }
+            
+            describe("when the list is empty") {
+                it("adds item to the subject") {
+                    expect(subject.peek()).to(equal(99))
+                }
+            }
+            
+            describe("when the list is NOT empty") {
+                beforeEach {
+                    subject.addToBack(item: 88)
+                }
+                
+                it("adds the item to the subject at the back of the linked list") {
+                    expect(subject.peek()).to(equal(99))
+                    
+                    // the easiest way to test that it's adding a node in the back of a list is to remove the item
+                    // immediately before it and then verify it's present
+                    
+                    _ = subject.removeFromFront()
+                    
+                    expect(subject.peek()).to(equal(88))
+                }
+            }
+        }
+        
+        describe("#removeFromBack()") {
+            var removedItem: Int!
+
+            describe("when the list is empty") {
+                beforeEach {
+                    removedItem = subject.removeFromBack()
+                }
+
+                it("returns nil") {
+                    expect(removedItem).to(beNil())
+                }
+            }
+
+            describe("when the list has one node") {
+                beforeEach {
+                    subject.addToBack(item: 12345)
+
+                    removedItem = subject.removeFromBack()
+                }
+                it("returns the item at the back of the linked list") {
+                    expect(removedItem).to(equal(12345))
+                }
+            }
+            
+            describe("when the list has more than one node") {
+                beforeEach {
+                    subject.addToBack(item: 12345)
+                    subject.addToBack(item: 777)
+                    
+                    removedItem = subject.removeFromBack()
+                }
+                it("returns the item at the back of the linked list") {
+                    expect(removedItem).to(equal(777))
+                }
             }
         }
     }
