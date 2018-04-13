2957c525e1a1eb5c01ace4ed7ad7cdbb020832e4
Ryan <github@ryan.codes>
2018-04-12 21:43:58 -0600

    Add StringChallenges

---
+++ Specs/Code Challenges/StringChallengesSpec.swift
@@ -0,0 +1,14 @@
+import Quick
+import Nimble
+
+@testable import AlgorithmsAndDataStructuresInSwift
+
+class StringChallengesSpec: QuickSpec {
+    override func spec() {
+        var subject: StringChallenges!
+        
+        beforeEach {
+            subject = StringChallenges()
+        }
+    }
+}
