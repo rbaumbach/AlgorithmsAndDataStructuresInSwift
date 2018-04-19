497e4cc55682d885440c20cf507c119a24ea6386
Ryan <github@ryan.codes>
2018-04-19 11:30:20 -0600

    Add breadthFirst and depthFirst traversal methods

--- AlgorithmsAndDataStructuresInSwift/Data Structures/Tree.swift
+++ AlgorithmsAndDataStructuresInSwift/Data Structures/Tree.swift
@@ -16,4 +16,44 @@ class Tree<T: Equatable> {
     func isEmpty() -> Bool {
         return root == nil
     }
+    
+    func traverseBreadthFirst() -> [T] {
+        guard let rootNode = root else {
+            return []
+        }
+        
+        var traversalArray: [TreeNode<T>] = [rootNode]
+        
+        var breadthFirstArray: [T] = []
+        
+        while traversalArray.count != 0 {
+            let currentNode = traversalArray.removeFirst()
+            
+            traversalArray += currentNode.children
+            
+            breadthFirstArray.append(currentNode.item)
+        }
+        
+        return breadthFirstArray
+    }
+    
+    func traverseDepthFirst() -> [T] {
+        guard let rootNode = root else {
+            return []
+        }
+        
+        var traversalArray: [TreeNode<T>] = [rootNode]
+        
+        var depthFirstArray: [T] = []
+        
+        while traversalArray.count != 0 {
+            let currentNode = traversalArray.removeFirst()
+            
+            traversalArray = currentNode.children + traversalArray
+            
+            depthFirstArray.append(currentNode.item)
+        }
+        
+        return depthFirstArray
+    }
 }
--- Specs/Data Structures/TreeSpec.swift
+++ Specs/Data Structures/TreeSpec.swift
@@ -34,5 +34,76 @@ class TreeSpec: QuickSpec {
                 }
             }
         }
+        
+        describe("traversal methods") {
+            var aPopulatedRoot: TreeNode<String>!
+            var traversalResult: [String]!
+            
+            beforeEach {
+                aPopulatedRoot = TreeNode<String>(item: "Uno")
+                let children = [TreeNode<String>(item: "Dos"), TreeNode<String>(item: "Tres"), TreeNode<String>(item: "Quatro")]
+                let grandChildrenA = [TreeNode<String>(item: "Cinco"), TreeNode<String>(item: "Seis")]
+                let grandChildrenB = [TreeNode<String>(item: "Siete")]
+                let grandChildrenC = [TreeNode<String>(item: "Ocho"), TreeNode<String>(item: "Nueve")]
+                
+                children[0].children = grandChildrenA
+                children[1].children = grandChildrenB
+                children[2].children = grandChildrenC
+                
+                aPopulatedRoot.children = children
+            }
+            
+            describe("#traverseBreadthFirst()") {
+                describe("when tree is empty") {
+                    beforeEach {
+                        traversalResult = subject.traverseBreadthFirst()
+                    }
+                    
+                    it("returns an empty array") {
+                        expect(traversalResult).to(equal([]))
+                    }
+                }
+                
+                describe("when the tree is NOT empty") {
+                    beforeEach {
+                        subject.root = aPopulatedRoot
+
+                        traversalResult = subject.traverseBreadthFirst()
+                    }
+                    
+                    it("returns an array of TreeNodes that have been assembed breadth first") {
+                        let expectedArray = ["Uno", "Dos", "Tres", "Quatro", "Cinco", "Seis", "Siete", "Ocho", "Nueve"]
+                        
+                        expect(traversalResult).to(equal(expectedArray))
+                    }
+                }
+            }
+            
+            describe("#traverseDepthFirst()") {
+                describe("when tree is empty") {
+                    beforeEach {
+                        traversalResult = subject.traverseDepthFirst()
+                    }
+                    
+                    it("returns an empty array") {
+                        expect(traversalResult).to(equal([]))
+                    }
+                }
+                
+                describe("when the tree is NOT empty") {
+                    beforeEach {
+                        subject.root = aPopulatedRoot
+                        
+                        traversalResult = subject.traverseDepthFirst()
+                    }
+                    
+                    it("returns an array of TreeNodes that have been assembed breadth first") {
+                        let expectedArray = ["Uno", "Dos", "Cinco", "Seis", "Tres", "Siete", "Quatro", "Ocho", "Nueve"]
+                        
+                        expect(traversalResult).to(equal(expectedArray))
+                    }
+                }
+            }
+        }
     }
 }
