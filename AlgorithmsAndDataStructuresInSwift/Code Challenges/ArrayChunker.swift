c0c0e0fcdcd47dd1784fe6e6e02a8bcb4e4cff51
Ryan <github@ryan.codes>
2018-04-19 18:55:54 -0600

    Minor refactor of current ArrayChunker challenge

--- AlgorithmsAndDataStructuresInSwift/Code Challenges/ArrayChunker.swift
+++ AlgorithmsAndDataStructuresInSwift/Code Challenges/ArrayChunker.swift
@@ -4,30 +4,40 @@ class ArrayChunker {
     // MARK: - Public Methods
     
     func chunk(array: [Int], chunkSize: Int) -> [[Int]] {
+        var chunkedArray: [[Int]] = []
+        
+        generateArrayChunks(inputArray: array, chunkSize: chunkSize) { chunkArray in
+            chunkedArray.append(chunkArray)
+        }
+        
+        return chunkedArray
+    }
+    
+    // MARK: - Private Methods
+    
+    private func generateArrayChunks(inputArray: [Int],
+                                     chunkSize: Int,
+                                     onChunkCreation: ([Int]) -> Void) {
         var chunkSizeCounter = 0
         
         var currentChunk: [Int] = []
         
-        var chunkedArray: [[Int]] = []
-        
-        for (index, number) in array.enumerated() {
+        inputArray.forEach { number in
             currentChunk.append(number)
             
             chunkSizeCounter += 1
             
             if chunkSizeCounter > chunkSize - 1 {
-                chunkedArray.append(currentChunk)
+                onChunkCreation(currentChunk)
                 
                 currentChunk = []
                 
                 chunkSizeCounter = 0
             }
-            
-            if index == array.count - 1 && !currentChunk.isEmpty {
-                chunkedArray.append(currentChunk)
-            }
         }
         
-        return chunkedArray
+        if !currentChunk.isEmpty {
+            onChunkCreation(currentChunk)
+        }
     }
 }
