edaae2fa9b57433211d426b3302a6b5c540fff75
Ryan <github@ryan.codes>
2018-05-03 13:23:07 -0600

    Add iterative version of matrix search in MatrixSortedSearch.swift

--- AlgorithmsAndDataStructuresInSwift/Code Challenges/MatrixSortedSearch.swift
+++ AlgorithmsAndDataStructuresInSwift/Code Challenges/MatrixSortedSearch.swift
@@ -11,7 +11,29 @@ class MatrixSortedSearch {
     }
     
     func iterativeSearch(matrix: [[Int]], number: Int) -> (Int, Int) {
-        return (-2, -2)
+        var start = 0
+        var end = matrix.count
+        
+        while start < end {
+            let mid = (start + end) / 2
+            
+            if number >= matrix[mid][0] && number <= matrix[mid][matrix[mid].count - 1] {
+                let nLocation = mid
+                let mLocation = matrix[mid].binarySearch(item: number)
+                
+                if mLocation == -1 {
+                    return (-1, -1)
+                }
+                
+                return (nLocation, mLocation)
+            } else if number < matrix[mid][0] {
+                end = mid
+            } else {
+                start = mid + 1
+            }
+        }
+        
+        return (-1, -1)
     }
     
     // MARK: - Private Methods
