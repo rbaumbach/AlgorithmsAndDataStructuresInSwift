80aa437316424af3ec4d5d672f8f5c8a1fa5e267
Ryan <github@ryan.codes>
2018-05-04 22:18:40 -0600

    Add additional Fibonacci method

--- AlgorithmsAndDataStructuresInSwift/Algorithms/Fibonacci.swift
+++ AlgorithmsAndDataStructuresInSwift/Algorithms/Fibonacci.swift
@@ -24,6 +24,40 @@ class Fibonacci {
     }
     
     func recursiveGenerate(upto: Int) -> [Int] {
-        return []
+        if upto < 0 {
+            return []
+        }
+        
+        if upto == 0 {
+            return [0]
+        } else if upto == 1 {
+            return [0, 1]
+        }
+        
+        var outputArray: [Int] = [0, 1]
+        
+        recursiveGenerate(upto: upto, outputArray: &outputArray)
+        
+        return outputArray
+    }
+    
+    func of(number: Int) -> Int {
+        if number < 2 {
+            return number
+        }
+        
+        return of(number: number - 2) + of(number: number - 1)
+    }
+    
+    // MARK: - Private Methods
+    
+    private func recursiveGenerate(upto:Int, outputArray: inout [Int]) {
+        if upto < 2 {
+            return
+        }
+        
+        outputArray.append(outputArray[outputArray.count - 2] + outputArray[outputArray.count - 1])
+        
+        recursiveGenerate(upto: (upto - 1), outputArray: &outputArray)
     }
 }
--- Specs/Algorithms/FibonacciSpec.swift
+++ Specs/Algorithms/FibonacciSpec.swift
@@ -6,6 +6,10 @@ import Nimble
 // Problem: Output the Fibonacci series as an array (including 0) upto the n-th entry in the fibonacci series.
 //          The fibonacci series is an ordering of numbers where each number is the sum of the preceeding two.
 //          ex: 0 -> [0], 1 -> [0, 1], 2 -> [0, 1, 1], ... , 10 -> [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+//
+// Additional Problem: Output the n-th entry in the fibonacci series.
+//                     ex: 4 -> 3
+//                     10 -> 55
 
 class FibonacciSpec: QuickSpec {
     override func spec() {
@@ -56,6 +60,14 @@ class FibonacciSpec: QuickSpec {
                     expect(subject.recursiveGenerate(upto:1)).to(equal([0, 1]))
                     expect(subject.recursiveGenerate(upto:10)).to(equal([0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]))
                 }
+            }
+        }
+        
+        describe("#of(n:)") {
+            it("generates an array of the fibonacci sequence output the number of times") {
+                expect(subject.of(number: 0)).to(equal(0))
+                expect(subject.of(number: 4)).to(equal(3))
+                expect(subject.of(number: 10)).to(equal(55))
             }
         }
     }
