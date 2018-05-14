cc9ddfe2504466db44369494c8b1f44ecfe68324
Ryan <github@ryan.codes>
2018-05-14 15:53:53 -0600

    Fix a couple bugs in AscendingValueDetector, add additional method for returning multiple ascensions

--- AlgorithmsAndDataStructuresInSwift/Code Challenges/AscendingValueDetector.swift
+++ AlgorithmsAndDataStructuresInSwift/Code Challenges/AscendingValueDetector.swift
@@ -18,6 +18,10 @@ class AscendingValueDetector: NSObject {
             i += 1
         }
         
+        if startIndex != -1 && input[input.count - 2] < input[input.count - 1] {
+            return [startIndex, input.count - 1]
+        }
+        
         return []
     }
     
@@ -40,6 +44,46 @@ class AscendingValueDetector: NSObject {
             i += 1
         }
         
+        if startIndex != -1 {
+            return [startIndex, endIndex]
+        }
+        
         return []
+    }
+    
+    func find(input: [String]) -> [[String: Int]] {
+        var allascensions: [[String: Int]] = []
+        
+        var startIndex = -1
+        var endIndex = -1
+        
+        var i = 0
+        
+        while i < input.count - 1 {
+            print("Value: \(input[i])")
+            if startIndex == -1 && input[i] < input[i + 1] {
+                startIndex = i
+                endIndex = i + 1
+            } else if input[i] < input[i + 1] {
+                endIndex = i + 1
+            } else if startIndex != -1 && input[i] > input[i + 1] {
+                let ascensionDict = ["startIndex": startIndex,
+                                     "endIndex": endIndex]
+                allascensions.append(ascensionDict)
+                
+                startIndex = -1
+                endIndex = -1
+            }
+            
+            i += 1
+        }
+        
+        if startIndex != -1 {
+            let ascensionDict = ["startIndex": startIndex,
+                                 "endIndex": endIndex]
+            allascensions.append(ascensionDict)
+        }
+        
+        return allascensions
     }
 }
--- Specs/Code Challenges/AscendingValueDetectorSpec.swift
+++ Specs/Code Challenges/AscendingValueDetectorSpec.swift
@@ -12,6 +12,8 @@ import Nimble
 
 class AscendingValueDetectorSpec: QuickSpec {
     override func spec() {
+        var inputArray: [String]!
+        
         var subject: AscendingValueDetector!
         
         beforeEach {
@@ -19,7 +21,6 @@ class AscendingValueDetectorSpec: QuickSpec {
         }
         
         describe("#findFirst(input:)") {
-            var inputArray: [String]!
             var firstAsendingValueIndicies: [Int]!
             
             describe("when the input array is empty") {
@@ -36,7 +37,7 @@ class AscendingValueDetectorSpec: QuickSpec {
             
             describe("when input array does NOT have any ascending values") {
                 beforeEach {
-                    inputArray = ["b", "c"]
+                    inputArray = ["b", "a"]
                     firstAsendingValueIndicies = subject.findFirst(input: inputArray)
                 }
                 
@@ -46,13 +47,28 @@ class AscendingValueDetectorSpec: QuickSpec {
             }
             
             describe("when input array has ascending values") {
-                beforeEach {
-                    inputArray = ["b", "a", "b", "z", "r", "f", "c", "a", "b", "c"]
-                    firstAsendingValueIndicies = subject.findFirst(input: inputArray)
+                describe("when the first ascension is in the middle of the array") {
+                    beforeEach {
+                        inputArray = ["b", "a", "b", "z", "r", "f", "c", "a", "b", "c"]
+                        firstAsendingValueIndicies = subject.findFirst(input: inputArray)
+                    }
+                    
+                    it("returns the start and end index of the first ascending letters") {
+                        expect(firstAsendingValueIndicies).to(equal([1, 3]))
+                    }
                 }
                 
-                it("returns the start and end index of the first ascending letters") {
-                    expect(firstAsendingValueIndicies).to(equal([1, 3]))
+                describe("when the first ascension goes all the way to the end of the array") {
+                    describe("when the last element is part of the ascension") {
+                        beforeEach {
+                            inputArray = ["b", "a", "b", "c", "d"]
+                            firstAsendingValueIndicies = subject.findFirst(input: inputArray)
+                        }
+                        
+                        it("returns the start and end index of the first ascending letters") {
+                            expect(firstAsendingValueIndicies).to(equal([1, 4]))
+                        }
+                    }
                 }
             }
         }
@@ -63,7 +79,6 @@ class AscendingValueDetectorSpec: QuickSpec {
         // ex: ["d", "c", "c", "c", "d", "e", "f", "f", "f", "g", "h", "h", "a"] -> [3, 10]
         
         describe("#findFirstTake2(input:)") {
-            var inputArray: [String]!
             var firstAsendingValueIndicies: [Int]!
 
             describe("when the input array is empty") {
@@ -80,7 +95,7 @@ class AscendingValueDetectorSpec: QuickSpec {
             
             describe("when input array does NOT have any ascending values") {
                 beforeEach {
-                    inputArray = ["b", "c"]
+                    inputArray = ["b", "a"]
                     firstAsendingValueIndicies = subject.findFirst(input: inputArray)
                 }
                 
@@ -90,13 +105,76 @@ class AscendingValueDetectorSpec: QuickSpec {
             }
             
             describe("when input array has ascending values") {
-                beforeEach {
-                    inputArray = ["d", "c", "c", "c", "d", "e", "f", "f", "f", "g", "h", "h", "a"]
-                    firstAsendingValueIndicies = subject.findFirstTake2(input: inputArray)
+                describe("when the first ascension is in the middle of the array") {
+                    beforeEach {
+                        inputArray = ["d", "c", "c", "c", "d", "e", "f", "f", "f", "g", "h", "h", "a"]
+                        firstAsendingValueIndicies = subject.findFirstTake2(input: inputArray)
+                    }
+                    
+                    it("returns the start and end index of the first ascending letters") {
+                        expect(firstAsendingValueIndicies).to(equal([3, 10]))
+                    }
                 }
                 
-                it("returns the start and end index of the first ascending letters") {
-                    expect(firstAsendingValueIndicies).to(equal([3, 10]))
+                describe("when the first ascension goes all the way to the end of the array") {
+                    beforeEach {
+                        inputArray = ["d", "c", "c", "c", "d", "e", "f", "f", "f", "g", "h", "h", "i", "i"]
+                        firstAsendingValueIndicies = subject.findFirstTake2(input: inputArray)
+                    }
+                    
+                    it("returns the start and end index of the first ascending letters") {
+                        expect(firstAsendingValueIndicies).to(equal([3, 12]))
+                    }
+                }
+            }
+        }
+        
+        // Extra wrinkle: Using the same wrinkle from findFirstTake2 (duplicates should be accounted for), write a method
+        // that finds all sequences of ascending values and returns them as a dictionary that contains the start and end indicies
+        // for each ascension.
+        // ex: ["d", "c", "c", "c", "d", "e", "f", "f", "f", "g", "h", "h", "a", "b", "c"] ->
+        //          [ ["startIndex": 3,
+        //             "endIndex": 10],
+        //            ["startIndex": 12,
+        //             "endIndex": 14] ]
+        
+        describe("#find(input:)") {
+            var allAscendingValueIndicies: [[String: Int]]!
+
+            describe("when the input array is empty") {
+                beforeEach {
+                    inputArray = []
+                    allAscendingValueIndicies = subject.find(input: inputArray)
+                }
+
+                it("returns an empty array") {
+                    expect(allAscendingValueIndicies).to(equal([]))
+                }
+            }
+
+            describe("when input array does NOT have any ascending values") {
+                beforeEach {
+                    inputArray = ["z", "y", "x", "w", "r", "a", "a"]
+                    allAscendingValueIndicies = subject.find(input: inputArray)
+                }
+
+                it("returns an empty array") {
+                    expect(allAscendingValueIndicies).to(equal([]))
+                }
+
+                describe("when input array has ascending values") {
+                    beforeEach {
+                        inputArray = ["d", "c", "c", "c", "d", "e", "f", "f", "f", "g", "h", "h", "a", "b", "c"]
+                        allAscendingValueIndicies = subject.find(input: inputArray)
+                    }
+
+                    it("returns an array of dictionaries that contain the start/end indices of a letter ascension") {
+                        let expectedArray = [ ["startIndex": 3,
+                                               "endIndex": 10],
+                                              ["startIndex": 12,
+                                               "endIndex": 14] ]
+                        expect(allAscendingValueIndicies).to(equal(expectedArray))
+                    }
                 }
             }
         }
